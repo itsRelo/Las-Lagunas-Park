753a9a553e22e09157d939cfb5719fa2e3593297
itsReloScripts <itsRelo97@gmail.com>
2025-11-02 21:37:08 +0100

    Add files via upload
    Las Lagunas Park A simpel Map for you Fivem Server! :)

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean' 
+
+games {'gta5'}  
+
+this_is_a_map 'yes'  
+
+Author 'itsReoScripts' 
+
+description 'itsReloScripts_Las LagunasPark'  
+
+version '1.0'
+dependency '/assetpacks'
